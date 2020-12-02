8ac740e810ed5cb56ad8e029b76025e4a612780e
Alessandro <alessandro.mancinelli89@gmail.com>
2020-12-02 16:52:56 +0100

    Milestone 1 first approach

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+/* 
+Riprodurre un'interfaccia stile Netflix utilizando Vue
+*/
+const API_KEY = 'a03a9596122b7a930963fc97f1e3e35d';
+
 const app = new Vue({
     el: '#root',
-    data:{}
+    data:{
+        films: [],
+        inputUser:''
+    },
+    methods: {
+        call() {
+            axios.get('https://api.themoviedb.org/3/search/movie/', { params: {
+               'api_key' : API_KEY,
+               query: this.inputUser 
+            }
+            }).then(element=> this.films = element.data.results);
+            this.inputUser = '';
+        }
+    }
 });
